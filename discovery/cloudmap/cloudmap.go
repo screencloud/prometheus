1450bbbd005fd58cba9ce89990d3f82ccf6f60cc
Declan Mc Candless <declan@screencloud.io>
2019-09-25 15:00:57 +0100

    adding marketing account details

--- discovery/cloudmap/cloudmap.go
+++ discovery/cloudmap/cloudmap.go
@@ -41,7 +41,7 @@ type AccountDetails struct {
 }
 
 var AccountsDetails = map[string]AccountDetails{
-    "685154231839": {Service: "marketing", Environment: "staging"},
+	"685154231839": {Service: "marketing", Environment: "staging"},
 	"973393464270": {Service: "marketing", Environment: "production"},
 	"882639863719": {Service: "studio", Environment: "edge"},
 	"385945872227": {Service: "studio", Environment: "staging"},
